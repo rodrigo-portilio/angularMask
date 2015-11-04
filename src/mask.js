d57aff2cfc8a78a0a32d3091c1624910156adcec
Rodrigo Santos Portilio <rodrigo.portilio@outlook.com>
2015-11-04 07:00:09 -0300

    1.0.5

--- src/mask.js
+++ src/mask.js
@@ -1,1 +1,37 @@
-"use stric";angular.module("skyang.mask",[]).directive("skyangMask",function(){return{scope:{ngModel:"=",skyangMask:"@"},replace:!0,link:function(a){a.$watch("ngModel",function(e,s){if(e!==s){var n,t,k,r,c="",g=e;for(k=0,r=1;r&&k<a.skyangMask.length;++k)switch(n=g.charAt(k),t=String(a.skyangMask).charAt(k),String(a.skyangMask).charAt(k)){case"9":/\d/.test(n)?c+=n:r=0;break;case"A":/[a-z]/i.test(n)?c+=n:r=0;break;case"N":/[a-z0-9]/i.test(n)?c+=n:r=0;break;case"X":c+=n;break;default:c+=t}a.ngModel=c}},!0)}}});
+'use stric';
+
+angular.module('skyang.mask', [])
+    .directive('skyangMask', function mask() {
+        return {
+            scope: {
+                ngModel: '=',
+                skyangMask: '@'
+            },
+            replace: true,
+            link: function(scope, element, attrs) {
+                scope.$watch('ngModel', function(newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        var text = '';
+                        var data = newValue;
+                        var c, m, i, x;
+
+                        for (i = 0, x = 1; x && i < scope.skyangMask.length; ++i) {
+                            c = data.charAt(i);
+                            m = String(scope.skyangMask).charAt(i);
+
+                            switch (String(scope.skyangMask).charAt(i)) {
+                                case '9' : if (/\d/.test(c)) {text += c;} else {x = 0;} break;
+                                case 'A' : if (/[a-z]/i.test(c)) {text += c;} else {x = 0;} break;
+                                case 'N' : if (/[a-z0-9]/i.test(c)) {text += c;} else {x = 0;} break;
+                                case 'X' : text += c; break;
+                                default  : text += m; break;
+                            }
+                        }
+
+                        scope.ngModel = text;
+                    }
+                }, true);
+            }
+        };
+
+    });
